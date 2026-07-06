3fd9928a9fdd24123bf4b194ba55a74595177861
pbhutka <parthbhutkamail@gmail.com>
2026-07-06 22:22:16 +0530

    adding cors origin

--- server/index.js
+++ server/index.js
@@ -8,9 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-    ],
+    origin: ["http://localhost:5173", "https://logposetempo.vercel.app/"],
   }),
 );
 app.use(express.json());
